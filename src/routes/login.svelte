415412c632e7712bf8b2f34de5c3d40a1c007632
Vishal Ghosh <vishal.ghosh28@gmail.com>
2022-02-24 02:49:34 +0530

    added redirect to login page

--- src/routes/login.svelte
+++ src/routes/login.svelte
@@ -1,6 +1,18 @@
 <script>
+import { goto } from '$app/navigation';
+
+import client from '$lib/supabase';
+
+import { onMount } from 'svelte';
+
 	import LoginForm from '../components/loginform.svelte';
 	import NavBar from '../components/nabvar.svelte';
+
+	onMount(()=>{
+		if(client.auth.user()){
+			goto("/")
+		}
+	})
 </script>
 
 <svelte:head>
